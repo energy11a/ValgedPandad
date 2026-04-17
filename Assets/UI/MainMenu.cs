1ce6c970eb59d8dbf495b0547218f55e480ac5b6
GameJam <jurgen.mangujamm@gmail.com>
2026-04-17 22:03:22 +0300

    Init ui

---
+++ Assets/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play(){
+        Debug.Log("Play");
+    }
+
+    public void Exit(){
+
+        Debug.Log("Tere");   
+
+        Application.Quit();
+    }
+}
