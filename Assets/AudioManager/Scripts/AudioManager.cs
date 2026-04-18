417dd713f7747c54336b17b4728703c62b6d9704
jurgenmangujamm-blip <jurgen.mangujamm@gmail.com>
2026-04-18 12:52:54 +0300

    Made UI and Audiomanger

---
+++ Assets/AudioManager/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+using JetBrains.Annotations;
+
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+    [SerializeField] AudioMixerGroup mixer;
+    public static AudioManager instance;
+
+    void Awake() 
+    {
+        CreateInstance();
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.spatialBlend = 0f;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = mixer;
+
+        }
+    }
+    void CreateInstance()
+    {
+        if (instance == null)
+        instance = this;
+    }
+    public void Play (string name)
+{
+    Sound s = Array.Find(sounds, sound => sound.name == name);
+    s.source.Play();
+}
+
+
+}
